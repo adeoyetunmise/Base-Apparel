88607c084b21cf4bb47099eb368ee537cc4d6362
adeoyetunmise <adeoyetunmise53@gmail.com>
2024-08-14 14:17:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function show(){
+
+    let input = userInput1.value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let icon = document.getElementById('icon');
+     
+     if (input == ''){
+    display.innerHTML = "Input a valid email"
+
+     }else if(emailPattern.test(input)){
+        display.innerHTML = "Valid Email Address"
+        userInput1.value = ''
+        
+        
+     }else{
+        display.innerHTML = "Please provide a valid email"
+        icon.style.display = 'inline'
+     }
+}
